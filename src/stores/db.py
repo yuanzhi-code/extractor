3bce099924f5f502300a1de41739aece6aa86298
srkmyk <woxqaq@gmail.com>
2025-05-24 13:17:44 +0800

    feat: init db (#5)
    * feat: init db
    
    * chore: update layout
    
    * chore: update
    
    * feat: introduce alembic to manage database schema migration
    
    * fix: typos
    
    * update er graph and migration scripts

---
+++ src/stores/db.py
@@ -0,0 +1,16 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+
+# 设置数据库 URI
+DATABASE_URI = 'sqlite:///extractor.db'
+
+# 创建数据库引擎
+engine = create_engine(DATABASE_URI, echo=True, pool_size=10)
+
+# 声明基类
+Base = declarative_base()
+
+# TODO add orm models definitions here
+
+# 创建所有表
+Base.metadata.create_all(engine)
