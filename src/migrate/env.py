be58d83d5ea52162ec5f71d2ec946abd328bb34d
woxQAQ <woxqaq@gmail.com>
2025-05-24 21:14:20 +0800

    chore: update migration config and add readme

--- src/migrate/env.py
+++ src/migrate/env.py
@@ -1,6 +1,6 @@
 from logging.config import fileConfig
 
-from sqlalchemy import engine_from_config
+from sqlalchemy import engine_from_config, true
 from sqlalchemy import pool
 
 from alembic import context
@@ -25,7 +25,6 @@ target_metadata = Base.metadata
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
-
 
 def run_migrations_offline() -> None:
     """Run migrations in 'offline' mode.
--- src/models/db.py
+++ src/models/db.py
@@ -1,16 +1,12 @@
+import os
 from sqlalchemy import MetaData, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 
+sqlite_path = os.path.abspath("../app.db")
+
 # 设置数据库 URI
-DATABASE_URI = 'sqlite:///extractor.db'
+DATABASE_URI = f'sqlite:///${sqlite_path}'
 
 # 创建数据库引擎
 engine = create_engine(DATABASE_URI, echo=True, pool_size=10)
 
-# 声明基类
-Base = declarative_base()
-
-# TODO add orm models definitions here
-
-# 创建所有表
-Base.metadata.create_all(engine)
